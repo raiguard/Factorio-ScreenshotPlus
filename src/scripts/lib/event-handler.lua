9a5fad5730fea5bc5cec83aea10463c6ddaf46c7
raiguard <caleb.heuer@gmail.com>
2019-11-22 21:39:35 -0700

    Add proper multiplayer support to event handler

--- src/scripts/lib/event-handler.lua
+++ src/scripts/lib/event-handler.lua
@@ -24,9 +24,8 @@ local bootstrap_handlers = {
 }
 
 -- register a handler for an event
--- when registering a conditional event, pass a unique conditional name as the third argument
--- additional_data is used internally by the handler, and is otherwise useless
-function event.register(id, handler, conditional_name)
+-- when registering a conditional event, pass a unique conditional name as the third argument and the player index as the fourth argument
+function event.register(id, handler, conditional_name, player_index)
     -- recursive handling of ids
     if type(id) == 'table' then
         for _,n in pairs(id) do
@@ -39,39 +38,50 @@ function event.register(id, handler, conditional_name)
         event_registry[id] = {}
     end
     local registry = event_registry[id]
-    -- make sure the handler has not already been registered
-    for _,t in ipairs(registry) do
-        if t.handler == handler then
-            -- don't register or insert handler
-            log('Tried to register event handler that already exists of id: '..id)
-            return
-        end
-    end
     -- create master handler if not already created
     if #registry == 0 then
         if type(id) == 'number' and id < 0 then
-            script.on_nth_tick(math.abs(id), event.dispatch)
+            script.on_nth_tick(-id, event.dispatch)
         elseif type(id) == 'string' and bootstrap_handlers[id] then
             script[id](bootstrap_handlers[id])
         else
             script.on_event(id, event.dispatch)
         end
     end
+    -- make sure the handler has not already been registered
+    for i,t in ipairs(registry) do
+        -- if it is a conditional event, 
+        if t.handler == handler and not conditional_name then
+            -- remove handler for re-insertion at the bottom
+            log('Re-registering existing event ID, moving to bottom')
+            table.remove(registry, i)
+        end
+    end
     -- add the handler to the events tables
     table.insert(registry, {handler=handler})
     if conditional_name then
+        assert(player_index, 'Must include player index when registering a conditional event')
         local con_registry = global.conditional_event_registry
         if not con_registry[conditional_name] then
-            con_registry[conditional_name] = {id={id}}
+            con_registry[conditional_name] = {id={id}, players={player_index}}
         else
-            table.insert(con_registry[conditional_name].id, id)
+            -- check if the ID has already been registered to this event
+            for _,id in ipairs(con_registry[conditional_name].id) do
+                if id == id then
+                    -- someone else already registered it, so add our player index to the list
+                    table.insert(con_registry[conditional_name].players, player_index)     
+                    return
+                end
+            end
+            -- insert the ID if it didn't exist already
+            table.insert(con_registry[conditional_name].id, id)     
         end
     end
 end
 
 -- deregisters a handler for an event
--- when deregistering a conditional event, pass its unique conditional name as the third argument
-function event.deregister(id, handler, conditional_name)
+-- when deregistering a conditional event, pass its unique conditional name as the third argument and the player index as the fourth argument
+function event.deregister(id, handler, conditional_name, player_index)
     -- recursive handling of ids
     if type(id) == 'table' then
         for _,n in pairs(id) do
@@ -85,23 +95,29 @@ function event.deregister(id, handler, conditional_name)
         log('Tried to deregister an unregistered event of id: '..id)
         return
     end
+    -- remove from conditional event registry if needed
+    if conditional_name then
+        local con_registry = global.conditional_event_registry[conditional_name]
+        for i,p in ipairs(con_registry.players) do
+            if p == player_index then
+                table.remove(con_registry.players, i)
+            end
+        end
+        if table_size(con_registry.players) == 0 then
+            global.conditional_event_registry[conditional_name] = nil
+        else
+            -- other players still need this conditional event, so don't do anything else
+            return
+        end
+    end
     -- remove the handler from the events tables
     for i,t in ipairs(registry) do
         if t.handler == handler then
             table.remove(registry, i)
         end
     end
-    if conditional_name then
-        local con_registry = global.conditional_event_registry[conditional_name]
-        for i,n in pairs(con_registry) do
-            if n == id then table.remove(con_registry, i) end
-        end
-        if #con_registry == 0 then
-            global.conditional_event_registry[conditional_name] = nil
-        end
-    end
     -- de-register the master handler if it's no longer needed
-    if #registry == 0 then
+    if table_size(registry) == 0 then
         if type(id) == 'number' and id < 0 then
             script.on_nth_tick(math.abs(id), nil)
         elseif type(id) == 'string' and bootstrap_handlers[id] then
@@ -148,8 +164,8 @@ function event.on_configuration_changed(handler)
 end
 
 -- shortcut for event.register(-nthTick, function)
-function event.on_nth_tick(nthTick, handler, conditional_name)
-    event.register(-nthTick, handler, conditional_name)
+function event.on_nth_tick(nthTick, handler, conditional_name, player_index)
+    event.register(-nthTick, handler, conditional_name, player_index)
 end
 
 --
@@ -177,11 +193,11 @@ local gui_event_filters = {
 local gui_event_data = {}
 
 -- registers event(s) for specific gui element(s)
-function event.gui.register(filters, id, handler, conditional_name)
+function event.gui.register(filters, id, handler, conditional_name, player_index)
     -- recursive handling of ids
     if type(id) == 'table' then
         for _,n in pairs(id) do
-            event.gui.register(filters, n, handler, conditional_name)
+            event.gui.register(filters, n, handler, conditional_name, player_index)
         end
         return
     end
@@ -200,26 +216,52 @@ function event.gui.register(filters, id, handler, conditional_name)
     table.insert(gui_event_data[id], {handler=handler, filters=filters})
     -- register conditional event if it is one
     if conditional_name then
+        assert(player_index, 'Must include player index when registering a conditional event')
         local con_registry = global.conditional_event_registry
         if not con_registry[conditional_name] then
-            con_registry[conditional_name] = {id={id}, filters=filters}
+            con_registry[conditional_name] = {id={id}, filters=filters, players={player_index}}
         else
-            table.insert(con_registry[conditional_name].id, id)
+            -- check if the ID has already been registered to this event
+            for _,id in ipairs(con_registry[conditional_name].id) do
+                if id == id then
+                    -- someone else already registered it, so add our player index to the list
+                    table.insert(con_registry[conditional_name].players, player_index)     
+                    return
+                end
+            end
+            -- insert the ID if it didn't exist already
+            table.insert(con_registry[conditional_name].id, id)     
         end
     end
 end
 
 -- deregisters event(s) from specific gui element(s)
-function event.gui.deregister(id, handler, conditional_name)
+function event.gui.deregister(id, handler, conditional_name, player_index)
     -- recursive handling of ids
     if type(id) == 'table' then
         for _,n in pairs(id) do
-            event.gui.deregister(n, handler, conditional_name)
+            event.gui.deregister(n, handler, conditional_name, player_index)
         end
         return
     end
+    -- remove from conditional event registry if needed
+    if conditional_name then
+        local con_registry = global.conditional_event_registry[conditional_name]
+        for i,p in ipairs(con_registry.players) do
+            if p == player_index then
+                table.remove(con_registry.players, i)
+                break
+            end
+        end
+        if table_size(con_registry.players) == 0 then
+            global.conditional_event_registry[conditional_name] = nil
+        else
+            -- other players still need this conditional event, so don't do anything else
+            return
+        end
+    end
     local data = gui_event_data[id]
-    -- remove the data from the data tables
+    -- remove the data from the data table
     for i,t in ipairs(data) do
         if t.handler == handler then
             table.remove(data, i)
@@ -228,7 +270,7 @@ function event.gui.deregister(id, handler, conditional_name)
     -- remove data table and deregister master handler if it is empty
     if #data == 0 then
         gui_event_data[id] = nil
-        event.deregister(id, event.gui.dispatch, conditional_name)
+        event.deregister(id, event.gui.dispatch)
     end
 end
 
@@ -274,8 +316,8 @@ local gui_event_shortcut_functions = {
 
 -- register shortcut functions
 for n,e in pairs(gui_event_shortcut_functions) do
-    event.gui[n] = function(filters, handler, conditional_name)
-        event.gui.register(filters, defines.events[e], handler, conditional_name)
+    event.gui[n] = function(filters, handler, conditional_name, player_index)
+        event.gui.register(filters, defines.events[e], handler, conditional_name, player_index)
     end
 end
 
